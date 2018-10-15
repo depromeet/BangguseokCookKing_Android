3e347ecb289337bc34628e5ea5062c181e5be941
leehoyong <lee199402@gmail.com>
2018-10-15 16:50:02 +0900

    * add recipe activity

---
+++ app/src/main/java/cooking/depromeet/github/com/bangguseokcookking_android/recipe/RecipeActivity.kt
@@ -0,0 +1,13 @@
+package cooking.depromeet.github.com.bangguseokcookking_android.recipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cooking.depromeet.github.com.bangguseokcookking_android.R
+
+class RecipeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe)
+    }
+}
