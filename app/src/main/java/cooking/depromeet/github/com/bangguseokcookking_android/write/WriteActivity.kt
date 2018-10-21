d3d4ba568d9b39cfc684df1d3c4f28f4f6de3906
leehoyong <lee199402@gmail.com>
2018-10-21 18:28:57 +0900

    * 글쓰기 화면 레이아웃 추가

--- app/src/main/java/cooking/depromeet/github/com/bangguseokcookking_android/write/WriteActivity.kt
+++ app/src/main/java/cooking/depromeet/github/com/bangguseokcookking_android/write/WriteActivity.kt
@@ -3,11 +3,30 @@ package cooking.depromeet.github.com.bangguseokcookking_android.write
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import cooking.depromeet.github.com.bangguseokcookking_android.R
+import kotlinx.android.synthetic.main.activity_write.*
+import org.jetbrains.anko.sdk25.coroutines.onRatingBarChange
 
 class WriteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
+
+        // cancel button
+        write_tv_cancel.setOnClickListener { finish() }
+
+        // submit btn
+        write_tv_submit.setOnClickListener {
+
+        }
+
+        // plus btn
+        write_ib_plus.setOnClickListener {
+
+        }
+
+        // rating bar
+
+
     }
 }
