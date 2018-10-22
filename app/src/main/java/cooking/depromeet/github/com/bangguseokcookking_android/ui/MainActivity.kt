d1878e1abfc311b8b49146d04f51f72d578ad7d7
leehoyong <lee199402@gmail.com>
2018-10-23 04:41:32 +0900

    navigation header
    * profile thumbnail circle crop
    * btn click event

--- app/src/main/java/cooking/depromeet/github/com/bangguseokcookking_android/ui/MainActivity.kt
+++ app/src/main/java/cooking/depromeet/github/com/bangguseokcookking_android/ui/MainActivity.kt
@@ -2,7 +2,12 @@ package cooking.depromeet.github.com.bangguseokcookking_android.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import cooking.depromeet.github.com.bangguseokcookking_android.R
 import cooking.depromeet.github.com.bangguseokcookking_android.ui.mypage.MypageActivity
@@ -16,6 +21,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
 
+    private val roundOptions = RequestOptions()
+        .circleCrop()
+        .diskCacheStrategy(DiskCacheStrategy.NONE)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +34,16 @@ class MainActivity : AppCompatActivity() {
 
         // navigation drawer
         navigationDrawer()
+
+        val nav_header = main_nav.getHeaderView(0)
+        val header_thumbnail = nav_header.findViewById<AppCompatImageView>(R.id.header_iv_profile)
+        val header_mypage = nav_header.findViewById<AppCompatTextView>(R.id.header_tv_mypage)
+        header_mypage.setOnClickListener { startActivity(intentFor<MypageActivity>()) }
+
+        Glide.with(this)
+            .load("https://file.namu.moe/file/f5d57681a95d89748f18b9ad3cbebeaf19392aa2f6bcb6820192301d611f3cc3")
+            .apply(roundOptions)
+            .into(header_thumbnail)
 
         // search
         main_et_search.setOnClickListener { startActivity(intentFor<SearchActivity>()) }
