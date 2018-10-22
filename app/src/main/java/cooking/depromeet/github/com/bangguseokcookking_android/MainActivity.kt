6cd02649e112dca4b202a48a43f30a982a01c81d
leehoyong <lee199402@gmail.com>
2018-10-22 17:44:01 +0900

    * 메인화면 drawer navigation 추가

--- app/src/main/java/cooking/depromeet/github/com/bangguseokcookking_android/MainActivity.kt
+++ app/src/main/java/cooking/depromeet/github/com/bangguseokcookking_android/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import cooking.depromeet.github.com.bangguseokcookking_android.write.WriteActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_drawer.*
 import org.jetbrains.anko.intentFor
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,11 @@ class MainActivity : AppCompatActivity() {
         // bottom sheet
         bottomSheetBehavior = BottomSheetBehavior.from(findViewById<ConstraintLayout>(R.id.main_bottom))
 
+
+        // navigation drawer
+        navigationDrawer()
+
+
         // 마이페이지
         main_ib_mypage.setOnClickListener {
 
@@ -29,5 +35,19 @@ class MainActivity : AppCompatActivity() {
         main_ib_write.setOnClickListener {
             startActivity(intentFor<WriteActivity>())
         }
+    }
+
+    private fun navigationDrawer() = main_nav.setNavigationItemSelectedListener {
+        main_drawer.closeDrawers()
+        when (it.itemId) {
+            R.id.menu_recipe -> {
+            }
+            R.id.menu_book_mark -> {
+            }
+            R.id.menu_like -> {
+            }
+        }
+
+        true
     }
 }
