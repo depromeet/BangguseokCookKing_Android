297c90e3a0aded44877bc27476338eb1faa32a39
leehoyong <lee199402@gmail.com>
2018-10-23 01:29:51 +0900

    * 액티비티 종료 추가

--- app/src/main/java/cooking/depromeet/github/com/bangguseokcookking_android/ui/login/LoginActivity.kt
+++ app/src/main/java/cooking/depromeet/github/com/bangguseokcookking_android/ui/login/LoginActivity.kt
@@ -9,7 +9,7 @@ import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.intentFor
 
 class LoginActivity : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(
