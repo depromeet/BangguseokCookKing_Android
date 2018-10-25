e37804949bfa3dd66f59d5d9763e1e3610113362
trycatch <trycatch98@gmail.com>
2018-10-25 16:32:20 +0900

    Revert "Revert "댓글, 관련 검색 리스트""
    This reverts commit 3009d0f14bf86098ac17d1a0afd6816908f296f0.

---
+++ app/src/main/java/cooking/depromeet/github/com/bangguseokcookking_android/recipe/CommentDataModel.kt
@@ -0,0 +1,8 @@
+package cooking.depromeet.github.com.bangguseokcookking_android.recipe
+
+data class CommentDataModel(
+        val profile: String,
+        val author: String,
+        val count: String,
+        val contents: String
+)
